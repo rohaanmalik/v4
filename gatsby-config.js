53af06d9b708517adc6592eb57bb26c62b984f39
rohaan malik <rohaanmalik7@gmail.com>
2021-08-12 08:16:44 -0400

    added logo & changed info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Rohaan Malik',
+    description: 'Rohaan Malik is a software engineer who specializes in full-stack engineering.',
+    siteUrl: 'https://rohaanmalik.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@rohaan7malik',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
